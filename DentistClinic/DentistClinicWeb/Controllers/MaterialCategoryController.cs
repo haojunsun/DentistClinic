0ded62a9fe487b92317d2271c26b780f95185550
haojun.sun <coolboss1@sina.com>
2015-12-30 16:55:36 +0800

    添加 控制器

---
+++ DentistClinic/DentistClinicWeb/Controllers/MaterialCategoryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DentistClinic.Core.Services;
+using DentistClinic.Infrastructure.Services;
+using DentistClinic.Web.Helpers;
+
+namespace DentistClinic.Web.Controllers
+{
+    public class MaterialCategoryController : ControllerHelper
+    {
+        private readonly IMaterialCategoryService _material;
+        private readonly IHelperServices _helperServices;
+
+        public MaterialCategoryController(IMaterialCategoryService material,  IHelperServices helperServices)
+        {
+            _material = material;
+            _helperServices = helperServices;
+        }
+
+        // GET: MaterialCategory
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Del()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ DentistClinic/DentistClinicWeb/Helpers/ControllerHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DentistClinic.Core.Services;
+using DentistClinic.Infrastructure.Services;
+
+namespace DentistClinic.Web.Helpers
+{
+    public class ControllerHelper : Controller
+    {
+        public ContentResult CResult(int statusCode, string message)
+        {
+            Response.StatusCode = statusCode;
+            Response.TrySkipIisCustomErrors = true;
+            return Content(message);
+        }
+
+
+        public ActionResult JumpUrl(string page, string message)
+        {
+            return Content("<script>alert('" + message + "');window.location.href='" + Url.Action(page) + "';</script>");
+        }
+
+    }
+}
